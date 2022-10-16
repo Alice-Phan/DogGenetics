4bc7fc4d415fb30ad10b459e08b250be0b624867
Alice-Phan <alicebe.phan@gmail.com>
2022-10-16 18:31:26 -0500

    Created DogGenetics java file, created class and initialized the variables

--- src/DogGenetics.java
+++ src/DogGenetics.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DogGenetics {
+import java.util.Random;
+import java.util.Scanner;
+
+public class DogGenetics {
+    public static void main(String[] args) {
+        // Declare String variable to hold the name of the user's dog
+        String stringName;
+
+        // Declare and initialize the Scanner
+        Scanner sc = new Scanner(System.in);
+        // Get input from the user
+        System.out.println("What is your dog's name?");
+        stringName = sc.nextLine();
+        System.out.println("Well then, I have this highly reliable report on " + stringName + "'s prestigious background right here.");
+    }
+
+
+
 }
---
+++ src/README.md
@@ -0,0 +1,5 @@
+## Todo
+
+1. Write a program that asks the user for the name of their dog
+2. Generate a fake DNA background report on the pet dog
+3. It should assign a random percentage to 5 dog breeds (that should add up to 100%)
